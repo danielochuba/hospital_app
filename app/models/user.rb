dbaa429f8a47bc93cd2279964a6d1ce1c704f0ba
Daniel Ochuba <danielochuba78@gmail.com>
2024-04-17 17:02:03 +0100

    write validations to pass test

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, :last_name, :role, presence: true
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: "home#index"
+  get "/home", to: "home#index"
   # Defines the root path route ("/")
   # root "articles#index"
 end
