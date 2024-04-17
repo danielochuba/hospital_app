4c180986424e383dec65b65acf2d71b52a6288f1
Daniel Ochuba <danielochuba78@gmail.com>
2024-04-17 17:11:00 +0100

    fix linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,4 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :first_name, :last_name, :role, presence: true
-
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
-    first_name { "John" }
-    last_name  { "Doe" }
+    first_name { 'John' }
+    last_name { 'Doe' }
     role { 'Receptionist' }
   end
 end
