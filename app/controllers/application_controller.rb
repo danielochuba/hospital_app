571575ebf08d02d3ae45428678bef06113541a81
Daniel Ochuba <danielochuba78@gmail.com>
2024-04-19 03:07:12 +0100

    handle authorization exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  rescue_from CanCan::AccessDenied do |exception|
+    respond_to do |format|
+      format.json { head :forbidden }
+      format.html { redirect_to root_path, alert: exception.message }
+    end
+  end
+
   protected
 
   def configure_permitted_parameters
