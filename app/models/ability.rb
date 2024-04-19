42672010cddcbfe4d103bcde9cb5379ecb6b6ddd
Daniel Ochuba <danielochuba78@gmail.com>
2024-04-19 03:07:45 +0100

    setup abilities of user roles

---
+++ app/models/ability.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new
+
+    can :read, Patient if user.doctor?
+
+    return unless user.receptionist?
+
+    can :manage, Patient
+  end
+end
